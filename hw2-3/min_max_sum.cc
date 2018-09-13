37d96b264affc2ef8a56f0a0e3f0ad4bbe856cea
2018008013 <revsic99@gmail.com>
2018-09-13 19:26:35 +0900

    Upload MinMaxSum - Find min max and sum

---
+++ hw2-3/min_max_sum.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+
+int main() {
+    int arr[5] = { 0, };
+    for (int i = 0; i < 5; ++i) {
+        scanf("%d", &arr[i]);
+    }
+
+    int min, max, sum;
+    min = max = sum = arr[0];
+
+    for (int i = 1; i < 5; ++i) {
+        min = min < arr[i] ? min : arr[i];
+        max = max > arr[i] ? max : arr[i];
+        sum += arr[i];
+    }
+
+    std::cout 
+        << "min: " << min << std::endl
+        << "max: " << max << std::endl
+        << "sum: " << sum << std::endl; 
+}
