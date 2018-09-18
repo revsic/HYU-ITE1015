7ee82b7e7c6f4fc487b49182876a4087f9909480
2018008013 <revsic99@gmail.com>
2018-09-18 15:07:54 +0900

    Upload Array Struct Point - Scale points in double

---
+++ hw3-1/array_struct_point.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+typedef struct {
+    int xpos;
+    int ypos;
+} Point;
+
+void scale2x(Point* pp) {
+    pp->xpos *= 2;
+    pp->ypos *= 2;
+}
+
+int main() {
+    Point points[3] = { 0, };
+
+    for (int i = 0; i < 3; ++i) {
+        std::cin >> points[i].xpos >> points[i].ypos;
+        scale2x(&points[i]);
+    }
+
+    for (int i = 0; i < 3; ++i) {
+        printf("[%d] %d %d\n", i, points[i].xpos, points[i].ypos);
+    }
+}
