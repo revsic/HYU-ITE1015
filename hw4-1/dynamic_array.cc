88800f936166b9c8c057e73666b3d0edd10287ef
2018008013 <revsic99@gmail.com>
2018-10-02 15:00:26 +0900

    Update DynamicArray - Allocate memory dynamically

---
+++ hw4-1/dynamic_array.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    int* arr = new int[n];
+    for (int i = 0; i < n; ++i) {
+        arr[i] = i;
+    }
+
+    for (int i = 0; i < n; ++i) {
+        std::cout << arr[i] << ' ';
+    }
+    std::cout << std::endl;
+
+    delete[] arr;
+}
