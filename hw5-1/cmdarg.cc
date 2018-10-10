5d05aa61dcd527860463d8f1f6c69e4f9e532640
2018008013 <revsic99@gmail.com>
2018-10-10 15:17:25 +0900

    Update cmdarg - Add command line arguments

---
+++ hw5-1/cmdarg.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* argv[]) {
+    int total = 0;
+    std::string concat;
+
+    for (int i = 1; i < argc; ++i) {
+        int parse = std::atoi(argv[i]);
+        
+        if (parse == 0) {
+            concat += argv[i];
+        }
+        else {
+            total += parse;
+        }
+    }
+
+    std::cout 
+        << concat << std::endl
+        << total << std::endl;
+
+    return 0;
+}
