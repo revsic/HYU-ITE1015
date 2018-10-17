22333ba6f4a0f62f5be46023ac732b0112ebc1c9
revsic <revsic99@gmail.com>
2018-10-17 23:52:15 +0900

    Update Simple Account - Simple Account simulation

---
+++ hw6-3/simple_account_main.cc
@@ -0,0 +1,95 @@
+#include "simple_account.h"
+
+#include <iostream>
+#include <sstream>
+
+int main() {
+    Manager manager;
+    auto no_account = [] {
+        std::cout << "Account does not exist" << std::endl;
+    };
+
+    auto log = [](std::string&& msg, int result) {
+        if (result == 0) {
+            std::cout << "Success: ";
+        }
+        else if (result == 1){
+            std::cout << "Failure: ";
+        }
+        std::cout << msg << std::endl;
+    };
+
+    while (true) {
+        char job = '\0';
+        std::cout << "Job?" << std::endl;
+        std::cin >> job;
+
+        switch (job) {
+        case 'N': {
+            int id = manager.GetSize();
+            std::stringstream stream;
+            stream << "Account for user " << id << " registered";
+
+            log(stream.str(), manager.NewAccount());
+            std::cout << manager.Log(id) << std::endl;
+            break;
+        }
+        case 'D': {
+            int id = 0, money = 0;
+            std::cin >> id >> money;
+
+            if (manager.GetSize() == 0) {
+                no_account();
+                continue;
+            }
+
+            std::stringstream stream;
+            stream << "Deposit to user " << id << ' ' << money;
+
+            log(stream.str(), manager.Deposit(id, money));
+            std::cout << manager.Log(id) << std::endl;
+            break;
+        }
+        case 'W': {
+            int id = 0, money = 0;
+            std::cin >> id >> money;
+
+            if (manager.GetSize() == 0) {
+                no_account();
+                continue;
+            }
+
+            std::stringstream stream;
+            stream << "Withdraw from user " << id << ' ' << money;
+
+            log(stream.str(), manager.Withdraw(id, money));
+            std::cout << manager.Log(id) << std::endl;
+            break;
+        }
+        case 'T': {
+            int src = 0, dst = 0, money = 0;
+            std::cin >> src >> dst >> money;
+
+            if (manager.GetSize() == 0) {
+                no_account();
+                continue;
+            }
+
+            std::stringstream stream;
+            stream << "Transfer from user " << src << " to user " << dst << ' ' << money;
+        
+            log(stream.str(), manager.Transfer(src, dst, money));
+            if (src > dst) {
+                std::swap(src, dst);
+            }
+            std::cout << manager.Log(src) << std::endl;
+            std::cout << manager.Log(dst) << std::endl;
+            break;
+        }
+        case 'Q':
+            return 0;
+        }
+    }
+
+    return 0;
+}
